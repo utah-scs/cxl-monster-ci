3ffca302625b151ef80d6bffc43fd79b9c154edd
Ryan Stutsman <stutsman@cs.utah.edu>
2023-05-18 13:45:55 -0600

    Tweaks to CI runner setup.

--- setup-github-ci-runner.sh
+++ setup-github-ci-runner.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-PROJDIR=/proj/sandstorm-PG0
+REPO=https://github.com/utah-scs/monster
+TOKEN_FILE=/proj/sandstorm-PG0/.github-ci-runner-token
 
 mkdir actions-runner && cd actions-runner
 curl -o actions-runner-linux-x64-2.304.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.304.0/actions-runner-linux-x64-2.304.0.tar.gz
 tar xzf ./actions-runner-linux-x64-2.304.0.tar.gz
-token=$(cat $PROJDIR/.github-ci-runner-token)
-./config.sh --url https://github.com/utah-scs/monster --token $token
+
+pushd actions-runner
+token=$(cat $TOKEN_FILE)
+./config.sh --url https://github.com/utah-scs/monster --token $token --unattended
+sudo ./svc.sh install
+sudo ./svc.sh start
+popd
+
+
