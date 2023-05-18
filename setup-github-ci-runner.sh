3620064f1e9638c9d9ece6541d06bbf024d30335
Ryan Stutsman <stutsman@cs.utah.edu>
2023-05-18 15:18:59 -0600

    Oops - forgot x permission on dir.

--- setup-github-ci-runner.sh
+++ setup-github-ci-runner.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+set -e
+
 REPO=https://github.com/utah-scs/monster
 TOKEN_FILE=/proj/sandstorm-PG0/.github-ci-runner-token
 
 cd /local
 mkdir actions-runner
-chmod 660 actions-runner
+chmod 770 actions-runner
 cd actions-runner
 curl -o actions-runner-linux-x64-2.304.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.304.0/actions-runner-linux-x64-2.304.0.tar.gz
 tar xzf ./actions-runner-linux-x64-2.304.0.tar.gz
