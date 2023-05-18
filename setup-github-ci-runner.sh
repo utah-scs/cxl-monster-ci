379c0acb675431b36bd3ab05bde98b30eebf6546
Ryan Stutsman <stutsman@cs.utah.edu>
2023-05-18 14:27:25 -0600

    Make permissions more liberal.

--- setup-github-ci-runner.sh
+++ setup-github-ci-runner.sh
@@ -3,6 +3,7 @@
 REPO=https://github.com/utah-scs/monster
 TOKEN_FILE=/proj/sandstorm-PG0/.github-ci-runner-token
 
+umask 002
 cd /local
 mkdir actions-runner && cd actions-runner
 curl -o actions-runner-linux-x64-2.304.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.304.0/actions-runner-linux-x64-2.304.0.tar.gz
