4c1069aeb6f8339064dedba640b2a480a455ddc4
Ryan Stutsman <stutsman@cs.utah.edu>
2023-05-18 14:47:19 -0600

    Another attempt at the permissions.

--- setup-github-ci-runner.sh
+++ setup-github-ci-runner.sh
@@ -3,9 +3,10 @@
 REPO=https://github.com/utah-scs/monster
 TOKEN_FILE=/proj/sandstorm-PG0/.github-ci-runner-token
 
-umask 002
 cd /local
-mkdir actions-runner && cd actions-runner
+mkdir actions-runner
+chmod 660 actions-runner
+cd actions-runner
 curl -o actions-runner-linux-x64-2.304.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.304.0/actions-runner-linux-x64-2.304.0.tar.gz
 tar xzf ./actions-runner-linux-x64-2.304.0.tar.gz
 
