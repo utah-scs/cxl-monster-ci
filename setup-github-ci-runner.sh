45be679ac19a2a36b28fb858ee074d9237378c34
Ryan Stutsman <stutsman@cs.utah.edu>
2023-05-18 14:13:00 -0600

    chdir to install runner since repo dir is RO.

--- setup-github-ci-runner.sh
+++ setup-github-ci-runner.sh
@@ -3,15 +3,13 @@
 REPO=https://github.com/utah-scs/monster
 TOKEN_FILE=/proj/sandstorm-PG0/.github-ci-runner-token
 
+cd /local
 mkdir actions-runner && cd actions-runner
 curl -o actions-runner-linux-x64-2.304.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.304.0/actions-runner-linux-x64-2.304.0.tar.gz
 tar xzf ./actions-runner-linux-x64-2.304.0.tar.gz
 
-pushd actions-runner
 token=$(cat $TOKEN_FILE)
 ./config.sh --url https://github.com/utah-scs/monster --token $token --unattended
 sudo ./svc.sh install
 sudo ./svc.sh start
-popd
-
 
